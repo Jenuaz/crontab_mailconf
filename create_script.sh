bcee46adb6af48b43c1bbcb31168bc09b03886a5
root <lisyak.yevhenii@gmail.com>
2018-08-25 21:46:38 +0000

    Done

--- create_script.sh
+++ create_script.sh
@@ -3,8 +3,8 @@ if [ ! -e \$BUP ]; then \n\
 	sudo cp \$TAG \$BUP \n\
 	exit 0 \n\
 fi \n\
-MD5M='md5sum \$TAG | awk \'{print \$1}\'' \n\
-MD5B='md5sum \$BUP | awk \'{print \$1}\'' \n\
+MD5M='md5sum \$TAG | awk '{print \$1}'' \n\
+MD5B='md5sum \$BUP | awk '{print \$1}'' \n\
 if ! cmp \"\$MD5M\" \"\$MD5B\"; then \n\
 	echo "Crontab was modified" | mail -s "Report" jenuaz@gmail.com \n\
 fi"
