696e8f1ef3fcbba67d2459a9390916ed7cfe3e4d
Yevhenii <Jenuaz@rambler.ru>
2018-08-26 01:56:53 +0300

    Update config_setting_server.sh

--- config_setting_server.sh
+++ config_setting_server.sh
@@ -1,6 +1,7 @@
 wtgo=/etc/ssmtp
 
-sed 's/root/\#root/' $wtgo/ssmtp.conf >> $wtgo/ssmtp.conf
-sed 's/\#From/From/' $wtgo/ssmtp.conf >> $wtgo/ssmtp.conf
-sed 's/mailhub=mail/mailhub=smtp.gmail.com/' $wtgo/ssmtp.conf >> $wtgo/ssmtp.conf
-sed 's/hostname/\#hostnmae/' $wtgo/ssmtp.conf >> $wtgo/ssmtp.conf
+#sed 's/root/\#root/' $wtgo/ssmtp.conf >> $wtgo/ssmtp.conf
+#sed 's/\#From/From/' $wtgo/ssmtp.conf >> $wtgo/ssmtp.conf
+#sed 's/mailhub=mail/mailhub=smtp.gmail.com/' $wtgo/ssmtp.conf >> $wtgo/ssmtp.conf
+#sed 's/hostname/\#hostnmae/' $wtgo/ssmtp.conf >> $wtgo/ssmtp.conf
+sed -i -e 's/root/\#root/' -e 's/\#From/From/' -e 's/mailhub=mail/mailhub=smtp.gmail.com/' -e 's/hostname/\#hostnmae/' $wtgo/ssmtp.conf
