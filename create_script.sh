6aef3018f77d92041d2b815a8184cd78eaaf1e6e
root <lisyak.yevhenii@gmail.com>
2018-08-25 22:15:24 +0000

    NO idea

--- create_script.sh
+++ create_script.sh
@@ -2,9 +2,9 @@ echo "TAG=/etc/crontab \n BUP=/etc/.crontab \n\
 if [ ! -e \$BUP ]; then \n\
 	sudo cp \$TAG \$BUP \n\
 	exit 0 \n\
-fi \n\
-MD5M='md5sum \$TAG | awk '{print \$1}'' \n\
-MD5B='md5sum \$BUP | awk '{print \$1}'' \n\
-if ! cmp \"\$MD5M\" \"\$MD5B\"; then \n\
+fi \n \
+MD5M=\`md5sum \$TAG | awk '{print \$1}'\` \n\
+MD5B=\`md5sum \$BUP | awk '{print \$1}'\`\n\
+if [ \"\$MD5M\" != \"\$MD5B\" ]; then 
 	echo "Crontab was modified" | mail -s "Report" jenuaz@gmail.com \n\
 fi" >> changes_comparator_crontab.sh
