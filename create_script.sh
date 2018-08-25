a97b15c3f752d217133287983de27673a1231222
root <lisyak.yevhenii@gmail.com>
2018-08-25 21:48:24 +0000

    Next

--- create_script.sh
+++ create_script.sh
@@ -7,4 +7,4 @@ MD5M='md5sum \$TAG | awk '{print \$1}'' \n\
 MD5B='md5sum \$BUP | awk '{print \$1}'' \n\
 if ! cmp \"\$MD5M\" \"\$MD5B\"; then \n\
 	echo "Crontab was modified" | mail -s "Report" jenuaz@gmail.com \n\
-fi"
+fi" >> changes_comparator_crontab.sh
